ee9d0ca73812dc266757c90998e5ddd04a589e25
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2019-01-18 21:42:18 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# d2header-vue-library
+# d2header library
+
+This is dhis2 header library written in vue.js to include in external applications in DHIS2 Application.
+
+##how to use
+
+- Copy the file DhisHeader.vue from src/components/d2header location
+- Use <d2header/> tag for including header in your Application
+- Refer App.vue file.
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid no-gutters main" style="overflow-x:hidden;background-color: #FFF3E0;">
+    <d2header/>
+  </div>
+</template>
+
+<script>
+import d2header from "./components/d2header/DhisHeader";
+
+export default {
+  name: "App",
+  components: {
+    d2header,
+  }
+};
+</script>
+<style lang="scss">
+  @import 'assets/scss/style';
+</style>
